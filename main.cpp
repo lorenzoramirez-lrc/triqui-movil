4cbbc882371776b992e2b04e3736bb3aada2711c
brewLux <luxfusan@gmail.com>
2026-09-07 16:20:58 -0500

    feat: added option where IA can begin - Added an option at the beginning where either IA or Player can begin a game - Minor visual changes

--- main.cpp
+++ main.cpp
@@ -2,54 +2,67 @@
 #include "minmax.h"
 #include "gamestate.h"
 
-int main(){
-    GameState game(X,O);
-    MinMax minmax(O);
-    game.printTitle();
-    game.printInstructions();
-    game.printBoard();
+int main() {
+	int IABegins;
 
-    while(!game.gameOver()){
-        Board board = game.getBoard();
+	std::cout << "Ingrese quien quiere comenzar: 0 para IA - 1 para Usuario: ";
+	std::cin >> IABegins;
 
-        if(!game.isIAPlayerTurn()){
-            int jugada;
-            std::cout << "Seleccione su jugada: ";
-            std::cin >> jugada;
+	Marks iaPlayer;
+	if (IABegins == 0) {
+		iaPlayer = X;
+	} else {
+		iaPlayer = O;
+	}
+	GameState game(X, iaPlayer);
+	MinMax minmax(iaPlayer);
 
-            if(jugada == 1){
-                int pos;
-                std::cout << "Ingrese casilla en la que poner: ";
-                std::cin >> pos;
-                if(!game.play((pos-1)/3, (pos-1)%3)){
-                    std::cout << "Jugada invalida, intenta de nuevo.\n";
-                }else{
-                    game.printBoard();
-                }
-            }else if(jugada == 2){
-                int pos, nuevaPos;
-                std::cout << "Que ficha quiere mover: ";
-                std::cin >> pos;
-                std::cout << "A donde la quiere mover: ";
-                std::cin >> nuevaPos;
-                if(!game.move((pos-1)/3, (pos-1)%3, (nuevaPos-1)/3, (nuevaPos-1)%3)){
-                    std::cout << "Movimiento invalido, intenta de nuevo.\n";
-                }else{
-                    game.printBoard();
-                }
-            }else{
-                std::cout << "Muchas gracias por jugar!\n";
-                break;
-            }
+	game.printTitle();
+	std::cout << std::endl;
+	game.printInstructions();
+	game.printBoard();
 
-        }else{
-            Board mejorJugada = minmax.minmaxAlgorithm(board);
-            game.setBoard(mejorJugada);
-            game.nextTurn();
-            game.printBoard();
-        }
-    }
+	while(!game.gameOver()) {
+		Board board = game.getBoard();
 
-    game.printWinner();
-    return 0;
+		if(!game.isIAPlayerTurn()) {
+			int jugada;
+			std::cout << "Seleccione su jugada: ";
+			std::cin >> jugada;
+
+			if(jugada == 1) {
+				int pos;
+				std::cout << "Ingrese casilla en la que poner: ";
+				std::cin >> pos;
+				if(!game.play((pos-1)/3, (pos-1)%3)) {
+					std::cout << "Jugada invalida, intenta de nuevo.\n";
+				} else {
+					game.printBoard();
+				}
+			} else if(jugada == 2) {
+				int pos, nuevaPos;
+				std::cout << "Que ficha quiere mover: ";
+				std::cin >> pos;
+				std::cout << "A donde la quiere mover: ";
+				std::cin >> nuevaPos;
+				if(!game.move((pos-1)/3, (pos-1)%3, (nuevaPos-1)/3, (nuevaPos-1)%3)) {
+					std::cout << "Movimiento invalido, intenta de nuevo.\n";
+				} else {
+					game.printBoard();
+				}
+			} else {
+				std::cout << "Muchas gracias por jugar!\n";
+				break;
+			}
+
+		} else {
+			Board mejorJugada = minmax.minmaxAlgorithm(board);
+			game.setBoard(mejorJugada);
+			game.nextTurn();
+			game.printBoard();
+		}
+	}
+
+	game.printWinner();
+	return 0;
 }
